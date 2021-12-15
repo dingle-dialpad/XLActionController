c4ec87d6426673b953b83788a7b64f477bf81fa9
Dave Ingle <49661040+dingle-dialpad@users.noreply.github.com>
2021-12-15 15:18:27 -0800

    Update Package.swift
    Fix exclude and resource warnings in package manifest

--- Package.swift
+++ Package.swift
@@ -26,8 +26,10 @@ let package = Package(
         .library(name: "XLActionControllerYoutube", targets: ["XLActionController_Youtube"])
     ],
     targets: [
-        .target(name: "XLActionController", path: "Source",
-                resources: [Resource.process("Resource/ActionCell.xib")]),
+        .target(name: "XLActionController", 
+                path: "Source", 
+                exclude: ["Info.plist"],
+                resources: [Resource.process("../Resource/ActionCell.xib")]),
         getExampleTarget(name: "Periscope"),
         getExampleTarget(name: "Skype"),
         getExampleTarget(name: "Spotify"),
